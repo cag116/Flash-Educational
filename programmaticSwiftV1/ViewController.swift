56ef9f9b16ed9e40f0c07c85c496df855bef7a6e
Christopher Guirguis <christopherguirguis@christophersmbp.home>
2016-12-16 22:08:24 -0500

    Initial Commit

---
+++ programmaticSwiftV1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  programmaticSwiftV1
+//
+//  Created by Christopher Guirguis on 12/16/16.
+//  Copyright © 2016 Christopher Guirguis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
